a91ce0e82c1ae2a096e448a455faf84411ac7d61
ArghyadipBiswas <107795593+ArghyadipBiswas@users.noreply.github.com>
2023-01-16 22:21:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,9 +8,13 @@ bot = Client(
   api_hash="f68fde1378da8f85a243f2ae57f2fcf9",
   bot_token="5378163079:AAHhtCs9RPaEmux-UAiQOo7KVDmFRwMnd4o"
 )
-message = Client.get_messages(5042525177, "/mirror *")
-link = message.replace('/mirror ', '')
+@bot.on_message(filters.command('start'))
+def command1(bot, message):
+    message.reply_text("i am alive.")
 
-async def main():
-    async with app:
-        await app.send_message("5042525177", link)
+@bot.on_message(filters.command('mirror'))
+def command2(bot, message):
+    message.reply_text(message.text[7:])
+
+
+bot.run()
